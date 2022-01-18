43ebbb73debb787360b249e2aaba915d6a1f77b9
GreenNeptune <silentwisdom75@gmail.com>
2022-01-18 16:26:20 +0200

    add routing to pages

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,13 +5,17 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { MatIconModule} from '@angular/material/icon';
+import { MatListModule} from '@angular/material/list';
+import { RouterModule } from "@angular/router";
 
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
+    RouterModule
   ],
   declarations: [
     HeaderComponent,
