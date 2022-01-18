0deecc0d88943552f9fb9c6d00d0d52fe28ccf6b
GreenNeptune <silentwisdom75@gmail.com>
2022-01-18 16:09:10 +0200

    add layout

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-setup'),
+      dir: require('path').join(__dirname, './coverage/user-mangement'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-setup';
+  title = 'User Management';
 }
